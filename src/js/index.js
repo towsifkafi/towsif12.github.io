a82824691e00872d9bbb3e9af386e1fd1e40d659
Towsif12 <kafitowsif@gmail.com>
2021-09-04 05:38:09 +0600

    Cursor And Avatar Movement

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,34 @@ $('#servers').hover(function() {
 }, function() {
     $('#dropdown-servers').css({ opacity: 0 })
 })
+
+$(document).ready(function() {
+
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+     
+    $(document).mousemove(function(e){
+      mouseX = e.pageX - 30;
+      mouseY = e.pageY - 30; 
+    });
+      
+    setInterval(function(){
+      xp += ((mouseX - xp)/6);
+      yp += ((mouseY - yp)/6);
+      $("#circle").css({left: xp+45 +'px', top: yp+55 +'px'});
+    }, 10);
+  
+  });
+
+  let box = document.querySelector(".avatarR");
+  let boxBoundingRect = box.getBoundingClientRect();
+  let boxCenter= {
+      x: boxBoundingRect.left + boxBoundingRect.width/2, 
+      y: boxBoundingRect.top + boxBoundingRect.height/2
+  };
+  
+  document.addEventListener("mousemove", e => {
+      let angle = Math.atan2(e.pageX - boxCenter.x, - (e.pageY - boxCenter.y) )*(180 / Math.PI);      
+      box.style.transform = `rotate(${angle+90}deg)`;  
+      console.log(angle+90)
+  })
