5bfd3a359c0d6cff179b7ceeaacd62ce3bdb102f
Towsif Kafi <kafitowsif@gmail.com>
2023-03-05 23:44:28 +0600

    .

--- src/routes/socials/+server.js
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit';
- 
-/** @type {import('./$types').RequestHandler} */
-
-export function GET({ url }) {
-  console.log(url.pathname)
-
-  return new Response(String(url.pathname))
-}
